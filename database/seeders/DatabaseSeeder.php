027d3d75ef89744f91494e43d2c18cdad0c2b89b
Saiful Islam <saifhd85@gmail.com>
2021-09-29 14:32:52 +0530

    some updates on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,19 @@ class DatabaseSeeder extends Seeder
     {
 
         // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name'=>'User1',
+            'password'=>Hash::make('password'),
+            'email'=>'user1@gmail.com'
+        ]);
+        User::factory()->create([
+            'name' => 'User2',
+            'password' => Hash::make('password'),
+            'email' => 'user2@gmail.com'
+        ]);
         User::factory(5)->create();
-        for($i = 1; $i < 6; $i++){
-            for($j = 1; $j < 6; $j++){
+        for($i = 1; $i < 8; $i++){
+            for($j = 1; $j < 8; $j++){
                 if($j !== $i){
                     Message::factory(5)->create([
                         'from_id'=>$i,
